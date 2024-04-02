dcaf86ee4d42a605778cb5304cf0aeef3e14adeb
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2024-04-02 10:18:59 +0000

    dockerfile - add label org.opencontainers.image.created

--- bin/update-versions.sh
+++ bin/update-versions.sh
@@ -5,9 +5,12 @@ set -e
 
 mvn versions:set -DnewVersion="$MAVEN_JAR_VERSION"
 
+imageCreatedDate=$(date --rfc-3339=ns)
+
 sed -i 'deploy/docker/Dockerfile'  -e 's@ARG APP_VERSION=.*@ARG APP_VERSION='"$MAVEN_JAR_VERSION"'@g'
 sed -i 'deploy/docker/Dockerfile'  -e 's@LABEL org.opencontainers.image.version=.*@LABEL org.opencontainers.image.version='"$DOCKER_VERSION"'@g'
 sed -i 'deploy/docker/Dockerfile'  -e 's@LABEL org.opencontainers.image.revision=.*@LABEL org.opencontainers.image.revision='"$RELEASE_VERSION"'@g'
+sed -i 'deploy/docker/Dockerfile'  -e 's@LABEL org.opencontainers.image.created=.*@LABEL org.opencontainers.image.created="'"$imageCreatedDate"'"@g'
 
 sed -i 'deploy/helm/hello-springboot-microservice/Chart.yaml' -e 's@appVersion:.*@appVersion: "'"$MAVEN_JAR_VERSION"'"@g'
 
