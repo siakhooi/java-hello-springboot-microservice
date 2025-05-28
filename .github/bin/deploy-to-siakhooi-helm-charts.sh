089c49f6f92a06640a45c2849942729f3f9a9b99
Siak Hooi <siakhooi@gmail.com>
2025-05-28 07:53:23 +0000

    use {{ .Chart.Name }} for selectors

--- .github/bin/deploy-to-siakhooi-helm-charts.sh
+++ .github/bin/deploy-to-siakhooi-helm-charts.sh
@@ -11,7 +11,7 @@ HELM_CHART_PACKAGE_FILE=$(basename "$PATH_TO_FILE")
 TMPDIR=$(mktemp -d)
 
 TARGETPATH=docs/$HELM_CHART_NAME
-TARGETURL=https://${PUBLISH_TO_GITHUB_HELM_CHARTS_TOKEN}@github.com/siakhooi/helm-charts.git
+TARGETURL=https://${PUBLISH_TO_GITHUB_REPO_TOKEN}@github.com/siakhooi/helm-charts.git
 TARGETBRANCH=main
 TARGETDIR=helm-charts-repo
 TARGET_GIT_EMAIL=$PROJECT_NAME@siakhooi.github.io
@@ -27,7 +27,7 @@ TARGET_COMMIT_MESSAGE="$PROJECT_NAME: Auto deploy [$(date)]"
   cd "$TARGETDIR"
   git remote set-url origin "$TARGETURL"
   git restore --staged .
-  mkdir -p $TARGETPATH
+  mkdir -p "$TARGETPATH"
   cp -v -f "$HELM_CHART_SOURCE_PATH" "$TARGETPATH/$HELM_CHART_PACKAGE_FILE"
   git add "$TARGETPATH/$HELM_CHART_PACKAGE_FILE"
   git status
